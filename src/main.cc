4fa8716fee165065b91cf23797c2300eec5065ca
Andre Detsch <andre.detsch@foundries.io>
2024-05-24 18:16:12 -0300

    WIP: Add new version of aklite daemon main code, using new API methods
    Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 #include <sys/file.h>
 #include <iostream>
+#include <memory>
 #include <thread>
 #include <unordered_map>
 
@@ -11,6 +12,7 @@
 #include <boost/interprocess/sync/file_lock.hpp>
 #include <boost/program_options.hpp>
 
+#include "aktualizr-lite/api.h"
 #include "aktualizr-lite/cli/cli.h"
 #include "crypto/keymanager.h"
 #include "helpers.h"
@@ -229,8 +231,56 @@ static data::ResultCode::Numeric do_app_sync(LiteClient& client) {
 
   return client.install(target);
 }
-
 static int daemon_main(LiteClient& client, const bpo::variables_map& variables_map) {
+  FileLock lock;
+  if (client.config.uptane.repo_server.empty()) {
+    LOG_ERROR << "[uptane]/repo_server is not configured";
+    return EXIT_FAILURE;
+  }
+  if (access(client.config.bootloader.reboot_command.c_str(), X_OK) != 0) {
+    LOG_ERROR << "reboot command: " << client.config.bootloader.reboot_command << " is not executable";
+    return EXIT_FAILURE;
+  }
+
+  client.importRootMetaIfNeededAndPresent();
+
+  std::shared_ptr<LiteClient> client_ptr{&client, [](LiteClient* /*unused*/) {}};
+  // Setting apply_lock parameter to false, since we already took the FileLock above
+  AkliteClient akclient{client_ptr, false, false};
+  akclient.CompleteInstallation();
+
+  Uptane::HardwareIdentifier hwid(client.config.provision.primary_ecu_hardware_id);
+  uint64_t interval = client.config.uptane.polling_sec;
+  if (variables_map.count("interval") > 0) {
+    interval = variables_map["interval"].as<uint64_t>();
+  }
+
+  while (true) {
+    auto current = akclient.GetCurrent();
+    LOG_INFO << "Active Target: " << current.Name() << ", sha256: " << current.Sha256Hash();
+    LOG_INFO << "Checking for a new Target...";
+    auto cir = akclient.GetTargetToInstall(nullptr, -1, "", current, false);
+    if (cir.selected_target.IsUnknown()) {
+      std::this_thread::sleep_for(std::chrono::seconds(interval));
+      continue;
+    }
+    LOG_INFO << "Going to install " << cir.selected_target.Name() << ". Reason: " << cir.reason;
+    // A target is supposed to be installed
+    auto install_result =
+        akclient.PullAndInstall(cir.selected_target, cir.reason, generate_correlation_id(cir.selected_target),
+                                InstallMode::All, nullptr, true, true);
+    if (akclient.RebootIfRequired()) {
+      // no point to continue running TUF cycle (check for update, download, install)
+      // since reboot is required to apply/finalize the currently installed update (aka pending update)
+      // If a reboot command is set in configuration, and is executed successfully, we will not get to this point
+      break;
+    }
+  }  // while true
+
+  return EXIT_SUCCESS;
+}
+
+static int daemon_main_original(LiteClient& client, const bpo::variables_map& variables_map) {
   FileLock lock;
   if (client.config.uptane.repo_server.empty()) {
     LOG_ERROR << "[uptane]/repo_server is not configured";
